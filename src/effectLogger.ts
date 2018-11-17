396a99b05af8b5b4cb694e63467ec93a086c03a8
Harshal Patil <harshal.rp@gmail.com>
2018-11-17 15:54:13 +0530

    Basic implementation for printing routes

--- src/effectLogger.ts
+++ src/effectLogger.ts
@@ -1,8 +1,49 @@
+import * as path from 'path';
 
 import { RouteEffect, RouteEffectGroup } from '@marblejs/core';
+import { table } from 'table';
 
-type RouteEffects = (RouteEffect | RouteEffectGroup)[]
+type Route = RouteEffect | RouteEffectGroup;
+type Routes = Route[];
 
-export function printAPIEffects(effects: RouteEffects) {
-    console.log(effects);
+interface FlatRoute {
+    path: string;
+    method: string;
+}
+
+export function printAPIEffects(effects: Routes) {
+    const flattenedRoutes = flatten(effects);
+    flattenedRoutes.sort((x, y) => sortPaths(x.path, y.path));
+
+    const printData = flattenedRoutes.map((x) => ([x.method, x.path]));
+
+    console.log(table(printData));
+
+    return flattenedRoutes;
+}
+
+function flatten(effects: Routes, prefix: string = '', routes: FlatRoute[] = []) {
+
+    effects.forEach((x) => {
+
+        const routePath = path.join(prefix, x.path);
+
+        if (isRouteEffectGroup(x)) {
+            flatten(x.effects, routePath, routes);
+        } else {
+            routes.push({ path: routePath, method: x.method.toUpperCase() })
+        }
+    });
+
+    return routes;
+}
+
+function isRouteEffectGroup(x: any): x is RouteEffectGroup {
+    return Array.isArray(x.effects) && Array.isArray(x.middlewares);
+}
+
+function sortPaths(a: string, b: string) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    return a > b ? 1 : b > a ? -1 : 0;
 }
--- test/hello.ts
+++ test/hello.ts
@@ -1,6 +1,31 @@
 import { printAPIEffects } from '../src/effectLogger';
+import { RouteEffect, RouteEffectGroup } from '@marblejs/core';
+
+const x: (RouteEffect | RouteEffectGroup)[] = [
+    { path: '/', method: 'GET', middlewares: [], effect: () => {} },
+
+    { path: '/user', middlewares: [], effects: [
+        { path: '/', method: 'GET', effect: () => {} },
+        { path: '/', method: 'POST', effect: () => {} } ,
+
+        { path: '/user', middlewares: [], effects: [
+            { path: '/', method: 'GET', effect: () => {} },
+            { path: '/', method: 'POST', effect: () => {} } ],
+        }]
+    },
+
+    { path: '/api', middlewares: [], effects: [
+        { path: '/', method: 'GET', effect: () => {} },
+        { path: '/', method: 'POST', effect: () => {} } ,
+
+        { path: '/user', middlewares: [], effects: [
+            { path: '/', method: 'GET', effect: () => {} },
+            { path: '/', method: 'POST', effect: () => {} } ],
+        }]
+    }
+];
 
 test('adds 1 + 2 to equal 3', () => {
-    console.log(printAPIEffects);
+    printAPIEffects(x);
     expect(3).toBe(3);
 });
