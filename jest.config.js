82e87c08779a8dc7bd93a18bb980f26119b5c6be
Harshal Patil <harshal.rp@gmail.com>
2018-12-18 13:40:40 +0530

    Setup code coverage and travis build

--- jest.config.js
+++ jest.config.js
@@ -15,10 +15,13 @@ module.exports = {
     // clearMocks: false,
 
     // Indicates whether the coverage information should be collected while executing the test
-    // collectCoverage: false,
+    // collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
     // collectCoverageFrom: null,
+    collectCoverageFrom: [
+        "src/**/*.(ts|tsx|js)",
+    ],
 
     // The directory where Jest should output its coverage files
     // coverageDirectory: null,
